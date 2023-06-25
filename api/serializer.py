592b114a2d578660b45e207ab886c614933434d2
Simer <simerwadhwa115@gmail.com>
2023-06-25 13:06:01 -0700

    Complete Company API

--- api/serializer.py
+++ api/serializer.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from .models import Company
 
 # Create serializers here
-class CompanySerializer(serializers.HyperlinkedModelSerializer()):
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()
     class Meta:
-        model: Company
-        fields: "__all__"
+        model = Company
+        fields = "__all__"
 
 
 
